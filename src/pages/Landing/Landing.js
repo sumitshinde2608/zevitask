d4e1226ede2b36988badf57944f34d21d3c1f7a6
sumitshinde2608 <shindesumit268@gmail.com>
2022-09-15 19:15:15 +0530

    search bar done

---
+++ src/pages/Landing/Landing.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Landing.css";
+import Logo from "./logo.png";
+// import SearchButton from "./search-button.png";
+
+const Landing = () => {
+  return (
+    <div className="main-container">
+      <div className="head-nav">
+        <img id="zevi-logo" src={Logo} alt="Logo" height="50px" width="100px" />
+      </div>
+      <div className="search-box">
+        <div className="searchbar">
+          <form>
+            <input
+              type="search"
+              placeholder="Search"
+              onFocus={() => {
+                console.log("focused");
+              }}
+            />
+            <input
+              type="button"
+              value="Search"
+              onClick={() => {
+                console.log("clicked");
+              }}
+            />
+            {/* </input> */}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
